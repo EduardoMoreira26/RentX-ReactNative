c301ee669b8e3dd81e610f8c77101e301847c1c6
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-03-07 21:31:54 -0300

    commit

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
 
-import { Container } from './styles';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
+import {
+  Container
+} from './styles';
 
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 100;
+  }
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: animation.value}
+      ]
+    }
+  });
+
   return (
     <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Splash" onPress={handleAnimationPosition} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  }
+})
